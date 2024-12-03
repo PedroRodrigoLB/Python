bb06062ce2383479bc2fe7e1d8477391b7246281
Pedro Rodrigo L B <pedro.rodrigo.lealbarros@outlook.com>
2024-12-03 16:14:56 -0300

    atualização

--- Banco de Dados e Peewee ORM em Pythion/database.py
+++ Banco de Dados e Peewee ORM em Pythion/database.py
@@ -1,6 +1,16 @@
+# O Object-Relational Mapping (ORM) utilizado foi o "peewee"
+
+# Importando todas as informações do "peewee"
+
 from peewee import *
 
+# Foi utilizado o SQLite como B.D por ser mais simples
+
+# Criando o B.D
 db = SqliteDatabase('freelancers.db')
+
+
+# Criando as Tabelas
 
 class Usuario(Model):
     nome = CharField()
--- Banco de Dados e Peewee ORM em Pythion/main.py
+++ Banco de Dados e Peewee ORM em Pythion/main.py
@@ -1,3 +1,9 @@
+# Realizando CRUD completo em um Banco de Dados (B.D)
+                # C REATE
+    # C R U D   # R EAD
+                # U PDATE
+                # D ELETE
+
 from database import db, Usuario, Anuncio
 
 db.connect()
@@ -6,75 +12,75 @@ db.create_tables([Usuario, Anuncio])
 
 # Crenado Usuarios para o B.D
 
-    # usuario = Usuario.create(nome="ProgramadorPython", email="teste@teste.com", senha="123456")
+usuario = Usuario.create(nome="ProgramadorPython", email="teste@teste.com", senha="123456")
 
-    # Usuario.create(nome="Pedro", email="pedro@teste.com", senha="123456")
-    # Usuario.create(nome="Rodrigo", email="rodrigo@teste.com", senha="123456")
-    # Usuario.create(nome="Luana", email="luana@teste.com", senha="123456"
+Usuario.create(nome="Pedro", email="pedro@teste.com", senha="123456")
+Usuario.create(nome="Rodrigo", email="rodrigo@teste.com", senha="123456")
+Usuario.create(nome="Luana", email="luana@teste.com", senha="123456"
 
 #---------------------------------------------------------------------------------
 
 # Confirmando os dados armazenados
 
-    # lista_usuarios = Usuario.select()
-    # print("Lista usuarios:")
+lista_usuarios = Usuario.select()
+print("Lista usuarios:")
 
-    # for u in lista_usuarios:
-    #     print("--", u.id, u.nome, u.email)
+for u in lista_usuarios:
+    print("--", u.id, u.nome, u.email)
 
 #---------------------------------------------------------------------------------
 
 # Obtendo um usuario atraves do "id"
 
-    # usuario1 = Usuario.get(Usuario.id == 1)
+usuario1 = Usuario.get(Usuario.id == 1)
 
-    # print("Usuario pelo id:", usuario1.id, usuario1.nome)
+print("Usuario pelo id:", usuario1.id, usuario1.nome)
 
 #---------------------------------------------------------------------------------
 
 # Realizando a busta atraves de um email
 
-    # rodrigo = Usuario.get(Usuario.email == "rodrigo@teste.com")
+rodrigo = Usuario.get(Usuario.email == "rodrigo@teste.com")
 
-    # print("Usuario: ", rodrigo.id, rodrigo.nome, rodrigo.email)
+print("Usuario: ", rodrigo.id, rodrigo.nome, rodrigo.email)
 
 #---------------------------------------------------------------------------------
 
 # Realizando update no B.D
 
-    # luana = Usuario.get(Usuario.email == "luana@teste.com")
-    # luana.nome = "Luana Python"
-    # luana.save()
+luana = Usuario.get(Usuario.email == "luana@teste.com")
+luana.nome = "Luana Python"
+luana.save()
 
-    # print("Luana atualizada: ", luana.nome)
+print("Luana atualizada: ", luana.nome)
 
 #---------------------------------------------------------------------------------
 
 # Testando o atributo unico para email onde não pode ter repetição
 
-    # print("Tentando criar um usuario com e-mail duplicado")
+print("Tentando criar um usuario com e-mail duplicado")
 
-    # try:
-    #     usuario_duplicado = Usuario.create(nome="Duplicado", email="teste@teste.com", senha="123456")
+try:
+    usuario_duplicado = Usuario.create(nome="Duplicado", email="teste@teste.com", senha="123456")
 
-    # except:
-    #     print("E-mail existente!")
+except:
+    print("E-mail existente!")
 
 #---------------------------------------------------------------------------------
 
 # Deletando um dado no B.D
 
-    # usuario_deletado = Usuario.get (Usuario.email == "teste@teste.com")
-    # usuario_deletado.delete_instance ()
+usuario_deletado = Usuario.get (Usuario.email == "teste@teste.com")
+usuario_deletado.delete_instance ()
 
-    # try:
-    #     Usuario.get (Usuario.email == "teste@teste.com")
-    # except:
-    #     print ("Usuario deletado!")
+try:
+    Usuario.get (Usuario.email == "teste@teste.com")
+except:
+    print ("Usuario deletado!")
 
 #---------------------------------------------------------------------------------
 
-# Criando um anúncio e praticar o uso da chave estrangeira
+# Criando um anúncio e praticando o uso da chave estrangeira
 
 luana = Usuario.get (Usuario.email == "luana@teste.com")
 
@@ -86,3 +92,26 @@ anuncio = Anuncio.create(
 )
 
 print ("Novo anuncio: ", anuncio.id, anuncio.titulo)
+    
+
+# Mais anuncios para compor o B.D
+
+Anuncio.create(usuario = luana, titulo = "Anuncio 1", descricao = "Os 7 habitos de pessoas altamente eficazes", valor = 5000)
+Anuncio.create(usuario = luana, titulo = "Anuncio 2", descricao = "Boas praticas de programação", valor = 5000)
+Anuncio.create(usuario = luana, titulo = "Anuncio 3", descricao = "O poder infinito da sua mente", valor = 10000)
+
+print ("anuncios da luana:")
+
+anuncios_luana = Anuncio.select().join(Usuario).where(Usuario.email == "luana@teste.com")
+# Tradução: Busque na tabela "Anuncio" incluindo a tabela "Usuarios" onde o email usuario seja igual a "luana@teste.com" 
+
+for a in anuncios_luana:
+    print ("--", a.id, a.titulo, a.valor)
+
+#---------------------------------------------------------------------------------
+
+# Deletando todos os dados da tabela "Anuncio"
+
+Anuncio.delete().execute()
+
+print("Quantidade de anuncios: ", Anuncio.select().count())
